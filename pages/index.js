78a22169d962ac4dc55350a304a6a9ef34cd5fae
Humayun Kabir <humayunkabir.cep@gmail.com>
2021-06-02 17:17:10 +0600

    WIP Home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Banner from '../components/home/Banner';
+import Feature from '../components/home/Feature';
 
 export default function Home() {
   return (
     <>
       <Banner />
+      {/* <Feature /> */}
     </>
   );
 }

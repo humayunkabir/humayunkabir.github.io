31d67f9d069074504cdad910bc28d81da818965e
Humayun Kabir <humayunkabir.cep@gmail.com>
2021-06-02 17:23:08 +0600

    path added for purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './components/home/**/*.{js,ts,jsx,tsx}',
     './resources/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
